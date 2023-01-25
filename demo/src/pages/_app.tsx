d4de87d5fce6e5451946971fb7b24b1e5c905dc5
Sunghyun Cho <mail@chosunghyun.com>
2023-01-25 12:11:32 -0800

    add SA

--- demo/src/pages/_app.tsx
+++ demo/src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script src='https://scripts.simpleanalyticscdn.com/latest.js' />
+      <noscript>
+        {/* eslint-disable @next/next/no-img-element */}
+        <img src='https://queue.simpleanalyticscdn.com/noscript.gif' alt='' referrerPolicy='no-referrer-when-downgrade' />
+      </noscript>
+      <Component {...pageProps} />
+    </>
+  )
 }
