23a314ec32ee0be2f8dcc40c2670bb317ce8cc69
Sunghyun Cho <mail@chosunghyun.com>
2023-01-25 16:04:23 -0800

    add ipsums

--- demo/src/pages/index.tsx
+++ demo/src/pages/index.tsx
@@ -12,8 +12,22 @@ const type = (past_state: string, hwalja_map: any, pressed: string, isEditing: b
   return past_state + hwalja_map[pressed]['']
 }
 
+const ipsum = () => {
+  const ipsums = [
+    '죽는 날까지 하늘을 우러러',
+    '한 점 부끄럼이 없기를',
+    '잎새에 이는 바람에도',
+    '나는 괴로워했다.',
+    '별을 노래하는 마음으로',
+    '모든 죽어 가는 것을 사랑해야지',
+    '그리고 나한테 주어진 길을 걸어가야겠다.',
+    '오늘 밤에도 별이 바람에 스치운다.',
+  ]
+  return ipsums[Math.floor(Math.random() * ipsums.length)]
+}
+
 export default function Home() {
-  const [state, changeState] = useState('')
+  const [state, changeState] = useState(ipsum())
   const [isEditing, setIsEditing] = useState(false)
   return (
     <>
